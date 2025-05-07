e2ec6a0f6916ed13fe01fe16de96a4c363868fc9
juan ramon <juanramoncornejo@gmail.com>
2025-05-07 19:12:07 -0400

    Corregido el metada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar"; // Asegúrate de que el path esté correcto
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/footer";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-// Configuración de la fuente Poppins
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["400", "600", "700"], // Puedes ajustar los pesos de la fuente según lo necesites
+  weight: ["400", "600", "700"],
 });
 
-export const metadata = {
+// ✅ Tipa el objeto metadata
+export const metadata: Metadata = {
   title: "Proscom",
   description:
     "Desarrollo web y soluciones seguras para tu negocio. En PROSCOM, innovamos contigo con tecnología confiable y eficiente.",
@@ -23,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${poppins.variable} bg-gray-50 text-gray-800`}>
-        <Navbar /> {/* Este es el componente Navbar */}
-        {children} {/* Aquí se renderizan los contenidos de las páginas */}
+        <Navbar />
+        {children}
         <Footer />
       </body>
     </html>
