467fafbe96c80da793a0e176b43a3e91ab13dd12
juan ramon <juanramoncornejo@gmail.com>
2025-06-07 18:32:03 -0400

    se agreag url api

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost','panel.jrdeveloper.cl',],
+    domains: [
+      "127.0.0.1",
+      "localhost",
+      "panel.jrdeveloper.cl",
+      "api.jrdeveloper.cl",
+    ],
   },
 };
 
 export default nextConfig;
-
